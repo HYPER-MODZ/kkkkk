51d36b3ed50c93a2d7d1ed4b3c6183bfd15a14d8
sdcoder11 <120669556+sdcoder11@users.noreply.github.com>
2022-12-15 17:23:28 +0000

    Add files via upload

---
+++ config.ts
@@ -0,0 +1,26 @@
+﻿const config: Config = {
+    botToken: '5778803513:AAGqTkyF--atxLBdKisq4xx6YXgFUvSiUW8',
+    keepFiles: {
+        compared: true,
+        input: true,
+        single: true,
+        video: false,
+    },
+    messages: {
+        blocked: 'The Chinese website has blocked the bot, too bad 🤷‍♂️',
+        bye:
+            '💚 Thanks For Using Bot 💚\n' +
+            'special Thanks For script Owner Imcsu',
+        hello: 'Hi 👋 Send me a photo to convert it into a 2D anime art\n 🌸 Be sure to join the channel @animebotc',
+        media: '@Anime\\qq\\Ai\\bot',
+        received: 'Photo has been received, please wait',
+    },
+    proxyUrl: 'socks5://HIA:HIA@211.140.252.244:7300',
+    sendMedia: {
+        compared: true,
+        single: true,
+        video: false,
+    },
+};
+
+export default config;
